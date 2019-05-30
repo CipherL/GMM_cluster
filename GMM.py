a57bd87ed490526af203308189998298c98f366c
ucasfei <ucasfei@huilingdeMacBook-Pro.local>
2019-05-30 21:12:22 +0800

    add a GMM python file

---
+++ GMM.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from pandas import DataFrame
+from sklearn.mixture import GaussianMixture
+
+if __name__ = "__main__":
+	dir = ""
+
