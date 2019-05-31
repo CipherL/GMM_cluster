121f31d2be17217f2cdec924ce0e8b61ff089253
Cioher <gable16@163.com>
2019-05-31 09:30:09 +0800

    add directory and check

--- GMM.py
+++ GMM.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 from pandas import DataFrame
 from sklearn.mixture import GaussianMixture
 
+
+
 if __name__ = "__main__":
-	dir = ""
+	dir = r"/d/Users/dell/AppData/Roaming/SPB_Data/RBFNN/Matlab_File"
+        print(dir)
 
